fff379bcbb1b2967c7ec46497f61bdbd2a78ff95
Rik Vanmechelen <rvanmech@pratt.edu>
2025-03-05 19:49:33 -0500

    adding session store, cookie store and flash messages

--- bookedin.js
+++ bookedin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const { credentials } = require('./config')
 const handlebars = require('express-handlebars').create({
   helpers: {
     eq: (v1, v2) => v1 == v2,
@@ -21,6 +22,8 @@ const handlebars = require('express-handlebars').create({
 });
 
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
 
 const indexRouter = require('./routes/index');
 const authorsRouter = require('./routes/authors');
@@ -32,8 +35,23 @@ const port = 3000;
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser(credentials.cookieSecret));
+app.use(expressSession({
+  secret: credentials.cookieSecret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+}));
+
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+  next()
+})
 
 
+
+// Application logic related
 app.use('/', indexRouter);
 app.use('/authors', authorsRouter);
 app.use('/books', booksRouter);
