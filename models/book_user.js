3d246ff69b416966735d2baa955321c7a9571cfc
Rik Vanmechelen <rvanmech@pratt.edu>
2025-03-12 20:02:38 -0400

    basic bookuser model

---
+++ models/book_user.js
@@ -0,0 +1,38 @@
+const booksUsers = [
+  {bookId: "0", userEmail: "rvanmech@pratt.edu", status: "finished"},
+  {bookId: "1", userEmail: "rvanmech@pratt.edu", status: "reading"}
+];
+
+exports.statuses = [
+  "todo","reading","finished"
+]
+
+exports.add = (bookUser) => {
+  booksUsers.push(bookUser);
+}
+
+exports.get = (bookId, userEmail) => {
+  return booksUsers.find((bu) => {
+    return bu.bookId === bookId && bu.userEmail === userEmail;
+  })
+}
+
+exports.allForUser = (userEmail) => {
+  return booksUsers.find((bu) => {
+    return bu.userEmail === userEmail;
+  })
+}
+
+exports.update = (idx, bookUser) => {
+  booksUsers[idx] = bookUser;
+}
+
+exports.upsert = (bookUser) => {
+  let idx = booksUsers.findIndex((bu)=>{
+    return bu.bookId === bookUser.bookId &&
+      bu.userEmail === bookUser.userEmail;
+  })
+  idx < 0 ? exports.add(bookUser) : exports.update(idx, bookUser);
+}
+
+
